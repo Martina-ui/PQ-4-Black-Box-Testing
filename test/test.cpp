c8bd3c60601d62f047edaa1186bc1a1d895071fc
Martina Delger <martidelger@gmail.com>
2026-06-05 19:34:58 +0000

    Height tests complete

--- test/test.cpp
+++ test/test.cpp
@@ -5,36 +5,22 @@
 
 using namespace std;
 
-// the syntax for defining a test is below. It is important for the name to be unique, but you can group multiple tests with [tags]. A test can have [multiple][tags] using that syntax.
-TEST_CASE("Example Test Name - Change me!", "[flag]"){
-	// instantiate any class members that you need to test here
-	int one = 1;
-
-	// anything that evaluates to false in a REQUIRE block will result in a failing test 
-	REQUIRE(one == 0); // fix me!
-
-	// all REQUIRE blocks must evaluate to true for the whole test to pass
-	REQUIRE(false); // also fix me!
-}
-
-TEST_CASE("Test 2", "[flag]"){
-	// you can also use "sections" to share setup code between tests, for example:
-	int one = 1;
-
-	SECTION("num is 2") {
-		int num = one + 1;
-		REQUIRE(num == 2);
-	};
-
-	SECTION("num is 3") {
-		int num = one + 2;
-		REQUIRE(num == 3);
-	};
-
-	// each section runs the setup code independently to ensure that they don't affect each other
-}
 
 // You are free to write as many tests as you want. Your credit for this
 // assignment will be based on the proportion of passing tests as well as the
 // number of mutants caught, so it might be a good idea to write many small,
 // focused tests over a few large, potentially buggy tests...
+
+TEST_CASE("Height of empty tree is 0", "[height]") {
+	GatorBST tree;
+	REQUIRE(tree.Height() == 0);
+}
+
+TEST_CASE("Height increases with insertions", "[height]") {
+	GatorBST tree;
+	tree.Insert(50, "Martina");
+	tree.Insert(30, "Mikael");
+	tree.Insert(70, "Austin");
+	tree.Insert(20, "David");
+	REQUIRE(tree.Height() == 3);
+}
